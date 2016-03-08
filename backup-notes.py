2fec2b082945c168f058965eca030f55ca3280f7
Cvetomird91 <cvetomirdenchev@gmail.com>
2016-03-09 00:12:04 +0200

    extended Python script

--- backup-notes.py
+++ backup-notes.py
@@ -4,15 +4,24 @@ import os
 import datetime
 import pathlib
 import re
+import sys, traceback
 
 user_home = os.getenv("HOME")
 drive_dir = '/pCloudDrive/'
 drive_path = user_home + drive_dir
+notes_path = drive_path + 'notes/'
 
+notes = os.listdir(notes_path)
 files = os.listdir(drive_path)
 available_backups = []
 
-for f in files:
-    matchObj = re.match(r'notes-\d{2}\.\d{2}\.\d{4}$', f)
+date = datetime.datetime.now()
+current_date = date.strftime('%d.%m.%Y')
+new_dir = 'notes-' + current_date
+
+for file in files:
+    matchObj = re.match(r'notes-\d{2}\.\d{2}\.\d{4}$', file)
     if matchObj:
-        print(f)
+        print(file)
+
+print(notes)
